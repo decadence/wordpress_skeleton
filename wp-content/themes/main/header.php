175670a14f7d81b807c0e0e43efb2b1a97dd4d4b
Victor Isadov <brahman63@mail.ru>
2014-07-04 20:55:24 +0400

    Основательная подготовка

--- wp-content/themes/main/header.php
+++ wp-content/themes/main/header.php
@@ -2,8 +2,18 @@
 <html>
 
 <head>
-	<title></title>
+<meta charset="utf-8" />
+<title></title>
+
+<link href="./css/reset.css" rel="stylesheet" />
+<link href="./css/style.css" rel="stylesheet" />
+
+<script src="./js/ready.js"></script>
 </head>
 
 
 <body>
+
+
+
+
