21c55c2a824017bf0f96b371e199fcba50c3faac
Victor Isadov <brahman63@mail.ru>
2014-09-25 23:07:31 +0400

    Удаление лишнего в Minify

--- min/groupsConfig.php
+++ min/groupsConfig.php
@@ -1,15 +1,4 @@
-<?php
-/**
- * Groups configuration for default Minify implementation
- * @package Minify
- */
-
-/** 
- * You may wish to use the Minify URI Builder app to suggest
- * changes. http://yourdomain/min/builder/
- *
- * See http://code.google.com/p/minify/wiki/CustomSource for other ideas
- **/
+<?
 
 return array(
     'js' => array(
@@ -21,3 +10,7 @@ return array(
 		'//css/style.css'
 	)
 );
+
+
+
+
