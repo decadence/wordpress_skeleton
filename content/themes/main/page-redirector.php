bf5c18b0dfe7b4846fd0a426a35ab64260b99e5b
Victor Isadov <isadov@rospartner.ru>
2015-05-06 16:53:29 +0300

    redirector

---
+++ content/themes/main/page-redirector.php
@@ -0,0 +1,25 @@
+<?
+/*
+	Template Name: Redirector
+	Шаблон страницы, которая перенаправляет на своего первого ребёнка или на главную, если детей нет.
+*/
+if (!defined("ABSPATH")){
+	exit;
+}
+
+$page_id = get_queried_object_id();
+
+$children = get_pages(array(
+	'parent' => $page_id,
+	'hierarchical' => false,
+	'sort_column' => 'menu_order',
+	'sort_order' => 'ASC'
+));
+
+$redirect_url = $children ? get_permalink($children[0]->ID) : '/';
+wp_redirect($redirect_url);
+exit;
+
+
+
+
