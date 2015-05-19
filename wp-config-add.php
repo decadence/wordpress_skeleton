78bea2c74cf8a45ffbceb2d327b63c242c177f4d
Victor Isadov <isadov@rospartner.ru>
2015-05-19 18:04:54 +0300

    Путь от корня

--- wp-config-add.php
+++ wp-config-add.php
@@ -2,7 +2,7 @@
 define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp');
 define('WP_HOME', 'http://' . $_SERVER['SERVER_NAME']);
 define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/content');
-define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/content');
+define('WP_CONTENT_URL', '/content');
 define('WP_DEFAULT_THEME', 'main');
 define('WPLANG', 'ru_RU');
 
