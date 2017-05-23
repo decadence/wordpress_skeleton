20030042292cc8a6187762e5b2670d0eb1897fd6
Victor Isadov <isadov@icewood.net>
2017-05-23 14:33:25 +0300

    Возвращаемся на обычную структуру WP

--- wp-config-add.php
+++ wp-config-add.php
@@ -1,11 +1,6 @@
 <?
-define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp');
-define('WP_HOME', 'http://' . $_SERVER['SERVER_NAME']);
-define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/content');
-define('WP_CONTENT_URL', '/content');
 define('WP_DEFAULT_THEME', 'main');
 define('WPLANG', 'ru_RU');
-
 
 // просматриваем ли мы сайт с локального компьютера
 define("LOCALHOST", $_SERVER["REMOTE_ADDR"] == "127.0.0.1");
@@ -14,9 +9,13 @@ define("WP_DEBUG", LOCALHOST);
 // активируем MySQLi
 define("WP_USE_EXT_MYSQL", false);
 
-
 // отключение автоматического обновления ядра, мы делаем это через Git
 define("WP_AUTO_UPDATE_CORE", false);
+
+/**
+ * Путь к корню проекта
+ */
+define("ROOT", __DIR__);
 
 
 
