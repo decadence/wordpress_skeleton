6cadd361335bc7f135d9879d8fb265983ca68206
Victor Isadov <isadov@icewood.net>
2017-08-05 22:00:13 +0300

    Режим технических работ

--- public/index.php
+++ public/index.php
@@ -1,4 +1,12 @@
 <?php
 define('WP_USE_THEMES', true);
 require("../vendor/autoload.php");
-require(dirname( __FILE__ ) . '/wp/wp-blog-header.php');
+$wordpressHeader = __DIR__ . '/wp/wp-blog-header.php';
+
+// проверяем наличие главного файла на случай,
+// если мы в процессе обновления через Composer
+if (file_exists($wordpressHeader)) {
+    require($wordpressHeader);
+} else {
+    require(__DIR__ . "/maintenance.php");
+}
---
+++ public/maintenance.php
@@ -0,0 +1,17 @@
+<?
+header("HTTP/1.1 503 Maintenance");
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Сайт на обслуживании</title>
+</head>
+<body>
+В данный момент на сайте проводятся технические работы. <br>
+Зайдите, пожалуйста, чуть позже.
+</body>
+</html>
