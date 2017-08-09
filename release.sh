eae21e8e8b110a8d00581584e64fc6a18cbce3f3
Victor Isadov <isadov@icewood.net>
2017-08-09 10:00:03 +0300

    Добавление WP CLI в composer.json Скрипт релиза с поддержкой режима обслуживания

--- release.sh
+++ release.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# путь к файлу обслуживания
+maintenance=public/wp/.maintenance
+echo "<?php \$upgrading = time();" > ${maintenance}
 git pull --no-edit
-composer install --no-dev
+composer install
+rm ${maintenance}
